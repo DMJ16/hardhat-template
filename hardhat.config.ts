276ec93870b94e69d2e9746ab4fbcba798e8a5bf
David Josephs <davidjosephs12@gmail.com>
2020-12-08 04:34:16 -0500

    added solidity optimizer to hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,23 +9,38 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://mainnet.infura.io/v3/${process.env.INFURA as string}`,
+        url: `https://mainnet.infura.io/v3/${
+          process.env.RPC_API_KEY as string
+        }`,
       },
     },
     // ropsten: {
-    //   url: `https://ropsten.infura.io/v3/${process.env.INFURA as string}`,
+    //   url: `https://ropsten.infura.io/v3/${process.env.RPC_API_KEY as string}`,
     //   accounts: [process.env.PRIVATE_KEY as string],
     // },
     // rinkeby: {
-    //   url: `https://rinkeby.infura.io/v3/${process.env.INFURA as string}`,
+    //   url: `https://rinkeby.infura.io/v3/${process.env.RPC_API_KEY as string}`,
     //   accounts: [process.env.PRIVATE_KEY as string],
     // },
     // kovan: {
-    //   url: `https://kovan.infura.io/v3/${process.env.INFURA as string}`,
+    //   url: `https://kovan.infura.io/v3/${process.env.RPC_API_KEY as string}`,
     //   accounts: [process.env.PRIVATE_KEY as string],
     // },
   },
-  solidity: "0.7.5",
+  solidity: {
+    version: "0.7.5",
+    settings: {
+      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+  },
 };
 
 export default config;

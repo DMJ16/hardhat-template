ba19b8e2b516442a99293563423da071af5aab7a
David Josephs <davidjosephs12@gmail.com>
2021-05-14 21:40:34 -0700

    Refactor config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,70 +7,57 @@ import "@nomiclabs/hardhat-etherscan";
 import { HardhatUserConfig } from "hardhat/config";
 require("dotenv").config();
 
-const URL = process.env.ALCHEMY_URL ?? "";
-const DEV_PRIVATE_KEY = process.env.DEV_PRIVATE_KEY ?? "";
-const GAS_REPORTER_ENABLED = process.env.REPORT_GAS ? true : false;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY ?? "";
+const COMPILER_SETTINGS = [
+  { version: "0.8.4" },
+  { version: "0.7.6" },
+  { version: "0.6.12" },
+].map((compilerOptions) => ({
+  ...compilerOptions,
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1_000_000,
+    },
+    metadata: {
+      bytecodeHash: "none",
+    },
+  },
+}));
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       forking: {
-        url: URL,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
         // blockNumber:
       },
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
     ropsten: {
-      url: URL,
-      accounts: [DEV_PRIVATE_KEY],
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
     rinkeby: {
-      url: URL,
-      accounts: [DEV_PRIVATE_KEY],
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
     kovan: {
-      url: URL,
-      accounts: [DEV_PRIVATE_KEY],
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
     },
   },
   solidity: {
-    compilers: [
-      {
-        version: "0.8.4",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.7.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: "0.6.12",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
+    compilers: COMPILER_SETTINGS,
   },
   gasReporter: {
     currency: "USD",
-    enabled: GAS_REPORTER_ENABLED,
+    enabled: process.env.REPORT_GAS ? true : false,
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
 
